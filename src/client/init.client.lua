339f174df8b6c5bd9eb963f8445051904e27f30d
xa1on <xalon.dzn@gmail.com>
2025-01-24 00:33:29 -0800

    feat: add/simplify instructions in ui

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -14,18 +14,21 @@ Viewport.AnchorPoint = Vector2.new(0.5, 0.5)
 Viewport.Size = UDim2.fromScale(0.3, 0.3)
 Viewport.Position = UDim2.fromScale(0.8, 0.5)
 
+savedReplay.RecordingStarted:Connect(function()
+	Player.PlayerGui.Info.Recording.TextTransparency = 0
+end)
+
+savedReplay.RecordingEnded:Connect(function()
+	Player.PlayerGui.Info.Recording.TextTransparency = 1
+end)
+
 UserInputService.InputEnded:Connect(function(input)
 	if input.UserInputType == Enum.UserInputType.Keyboard then
 		if input.KeyCode == Enum.KeyCode.R then
 			savedReplay:StartRecording()
 		elseif input.KeyCode == Enum.KeyCode.T then
 			savedReplay:StopRecording()
-		elseif input.KeyCode == Enum.KeyCode.Y then
-			savedReplay:StartReplay(1)
-		elseif input.KeyCode == Enum.KeyCode.U then
-			savedReplay:StopReplay()
-		elseif input.KeyCode == Enum.KeyCode.P then
-			savedReplay:GoToFrame(1,0)
+			savedReplay:ShowReplay()
         end
 	end
 end)
